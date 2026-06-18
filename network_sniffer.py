f55f5ebfb5dc17ae7f6fc28e604be8ea04656d48
Saikumar Goriparthi <Kumarsai5810@gmail.com>
2026-06-18 19:22:06 +0530

    Updated network sniffer

--- network_sniffer.py
+++ network_sniffer.py
@@ -7,6 +7,7 @@ def process_packet(packet):
     packet_count += 1
 
     if packet.haslayer(IP):
+        packet.show()
         print(f"\nPacket #{packet_count}")
         print(f"Source IP      : {packet[IP].src}")
         print(f"Destination IP : {packet[IP].dst}")
@@ -17,7 +18,15 @@ def process_packet(packet):
             print("Protocol       : UDP")
         elif packet.haslayer(ICMP):
             print("Protocol       : ICMP")
+        else:
+            print("Protocol       : Other")
 
         print(f"Packet Size    : {len(packet)} bytes")
 
-sniff(count=20, prn=process_packet, store=False)
+        if packet.haslayer(Raw):
+            payload = packet[Raw].load[:100]  # First 50 bytes
+            print(f"Payload        : {payload}")
+        else:
+            print("Payload        : No Raw Payload")
+
+sniff(count=100, prn=process_packet, store=False)  
